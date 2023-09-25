cf6ca8b3a8e227f5beb01fb58a46df2bd822a861
Roshali <143102316+roshali28@users.noreply.github.com>
2023-09-25 22:28:25 +0530

    Add files via upload
    length of longest substring

--- longest_substring.java
+++ longest_substring.java
@@ -1,22 +1,26 @@
 import java.util.*;
-public class longest_substring {
-	 public static int lengthOfLongestSubstring(String s) {
-		 
-	        char ch[]=s.toCharArray();
-	        char ch1[]=new char[s.length()];
-	        int j=0;
-	        for(int i=1;i<ch.length;i++){
-            	if(ch[i] != ch[i-1]) {
-            		ch1[j++]=ch[i];
-            	}
-	        }
-	        int count=ch1.length;
-	        return count;
+public class longest_substring {	
+ public static int lengthOfLongestSubstring(String s) {
+	 HashSet<Character> h = new HashSet<>();
+     int start=0;
+     int end=0;
+     int count=0; 
+     while(end < s.length()){
+      if(h.add(s.charAt(end))){
+          end++;
+          count=Math.max(count,h.size());
+      }
+      else{
+          h.remove(s.charAt(start));
+          start++;
+      }
+     }
+     return count; 
 	    }
-	public static void main(String[] args) {
-	  String s="pwwkew";
-	 int ans = lengthOfLongestSubstring(s);
-	 System.out.println(ans); 
+ public static void main(String[] args) { 
+	     int y =lengthOfLongestSubstring("pwwkew");
+	     System.out.println(y);
+	 
 	}
 
 }
