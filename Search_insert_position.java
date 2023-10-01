d7f8afd70e3144222f62dc12503dfe9e39d936cc
Roshali <143102316+roshali28@users.noreply.github.com>
2023-10-01 20:53:54 +0530

    Update Search_insert_position.java

--- Search_insert_position.java
+++ Search_insert_position.java
@@ -1,11 +1,13 @@
 import java.util.*;
 public class Search_insert_position {
 	public static int searchInsert(int[] nums, int target) {
+	     int n=0;
 	     for(int i=0;i<nums.length;i++){
 	       if(target <= nums[i])
 	       return i;
 	     }
-	     return nums.length;
+	     n=nums.length;
+	     return n;
 	    }
 	public static void main(String[] args) {
 	int nums[]= {1,3,5,6};	
